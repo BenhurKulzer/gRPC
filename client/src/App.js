e3027692ede050971a2eed04bc075555e26c2010
Benhur Kulzer <benhur.bitpc@gmail.com>
2024-10-02 00:04:07 -0300

    feat: changes in proto, app arch and dockerized everything to deploy, almost there

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
 import { useState } from 'react';
 
+import { GreeterClient } from "./protos/greeting_grpc_web_pb";
+import { GreetRequest } from "./protos/greeting_pb";
+
 import LogoSvg from './assets/logo';
+
+const greetClient = async (name) => {
+  const EnvoyURL = "http://localhost:8000";
+  const client = new GreeterClient(EnvoyURL);
+  const request = new GreetRequest();
+  request.setName(name);
+
+  const response = await client.greet(request, {});
+
+  const div = document.getElementById("response");
+  if (div) div.innerText = response.getMessage();
+};
 
 function App() {
   const [inputValue, setInputValue] = useState('');
 
-  function handleSubmitText(data: string) {
+  function handleSubmitText(data) {
     setInputValue(data);
+    if (data) greetClient(data);
   }
 
   return (
@@ -14,15 +30,16 @@ function App() {
       <div className="flex flex-col justify-center items-center text-center">
         <p className="text-base animate-pulse font-semibold text-indigo-600">gRPC Project</p>
         
-        <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">Hello {inputValue && inputValue}</h1>
+        <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">Hello {inputValue}</h1>
         <p className="mt-6 text-base leading-7">Inform a username to continue:</p>
+        <div id="response"></div>
         
         <div className="mt-10 flex items-center justify-center gap-x-6">
           <form
             className='flex gap-4'
             onSubmit={(e) => { 
               e.preventDefault();
-              handleSubmitText((e.target as HTMLFormElement).querySelector('input')?.value || '');
+              handleSubmitText(e.target.querySelector('input')?.value);
             }}
           >
             <input
@@ -40,7 +57,7 @@ function App() {
 
       <LogoSvg />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ server/server.js
@@ -0,0 +1,24 @@
+import grpc from '@grpc/grpc-js';
+import protoLoader from '@grpc/proto-loader';
+
+const packageDefinition = protoLoader.loadSync('../protos/greeting.proto', {});
+const userProto = grpc.loadPackageDefinition(packageDefinition).greeting;
+
+function doSayHello(call, callback) {
+  callback(null, { message: "Hello " + call.request.name + "!" });
+}
+
+function main() {
+  const server = new grpc.Server();
+
+  server.addService(userProto.Greeter.service, {
+    greet: doSayHello,
+  });
+  
+  server.bindAsync('127.0.0.1:50051', grpc.ServerCredentials.createInsecure(), (err, port) => {
+    console.log(`Server running at http://127.0.0.1:${port}`);
+    server.start();
+  });
+}
+
+main();
